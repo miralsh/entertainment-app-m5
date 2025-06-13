2058454096b7bd073d74dfdcfffa34c67d3fad75
miralsh <miralharsora18@gmail.com>
2025-06-13 17:12:17 +0530

    bookmarked

--- Client/src/components/Bookmarked.jsx
+++ Client/src/components/Bookmarked.jsx
@@ -153,37 +153,9 @@ const Bookmarked = ({ val }) => {
     return (
         <div className='w-full lg:mx-6 '>
             {val?.trim() != '' && search_bkmark && search_bkmark.length > 0 ?
-                (<> <h2 className="text-white text-2xl lg:mx-6">Bookmarked Movies</h2><div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  md:gap-4 gap-1 mt-6'>
+                (<> <h2 className="text-white text-2xl ">Bookmarked Movies</h2>
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  md:gap-4 gap-1 mt-6'>
                     {listToRender ? listToRender.filter(e => e.media_type == "movie").length != 0 ? listToRender.filter(e => e.media_type == "movie").map((element, index) => (
-                        // <div>
-                        //     {/* movie poster img */}
-                        //     <div key={index} className='w-[90%] me-4 bg-cover rounded-xl h-45 my-2 cursor-pointer hover:shadow-sm hover:shadow-white' style={{ backgroundImage: `url(${element.backdrop_path != null ? img_url + element.backdrop_path : 'https://images.pexels.com/photos/159868/lost-cat-tree-sign-fun-159868.jpeg'})` }} onClick={() => onSelect(element)}>
-                        //         {/* bookmark */}
-                        //         <div className='group flex justify-end pt-2 pe-2'>
-                        //             <div className="flex p-1 w-8 h-8 rounded-full bg-gray-700 opacity-75 cursor-pointer items-center justify-center group-hover:bg-white" onClick={(e) => { e.stopPropagation(); onBookMark(element) }}>
-                        //                 {bookmark.find(e => e.id == element.id) ? (<RiBookmarkFill className={`group-hover:text-black  text-white `} />) : (<RiBookmarkLine className={`group-hover:text-black  text-white `} />)}
-                        //             </div>
-                        //         </div>
-
-                        //     </div>
-                        //     {/* content */}
-                        //     <div className='flex flex-col'>
-                        //         <div className='flex items-center'>
-                        //             <p className='flex  items-center text-gray-200 py-1 md:ps-4 md:pe-2 text-xs md:text-sm px-1 '>{getYear(element.release_date)}</p>
-                        //             <hr className=" w-1 border-white border-dotted border-t-4" />
-                        //             <p className='flex items-center md:px-2 text-xs md:text-sm text-gray-200 capitalize'><RiFilmFill className='px-1' size={26} style={{ color: 'white' }} /> Movie</p>
-                        //             <hr className=" w-1 border-white border-dotted border-t-4 mx-1" />
-                        //             <p className='flex items-center md:px-2 text-xs md:text-sm text-gray-200 capitalize'>{certificationMap[element.id] || 'Loading...'}</p>
-                        //         </div>
-                        //         <p className='flex  md:text-xl items-center text-white w-[95%]  font-medium md:px-4 pb-4 px-1 overflow-hidden text-ellipsis whitespace-nowrap' style={{
-                        //             overflow: 'hidden',
-                        //             display: '-webkit-box',
-                        //             WebkitBoxOrient: 'vertical',
-                        //             WebkitLineClamp: 2
-                        //         }}>
-                        //             {element.title}{element.name}</p>
-                        //     </div>
-                        // </div>
                         <BookmarkCard key={index}
                             element={element}
                             isMovie={true}
@@ -198,34 +170,6 @@ const Bookmarked = ({ val }) => {
                     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-4 gap-1 mt-6 '>
                         {/* display bookmarked tv series content */}
                         {listToRender ? listToRender.filter(e => e.media_type == "tv").length != 0 ? listToRender.filter(e => e.media_type == "tv").map((element, index) => (
-                            //                     <div>
-                            //                         {/* tv series poster img */}
-                            //                         <div key={index} className='w-[90%] me-4 bg-cover rounded-xl h-45 my-2' style={{ backgroundImage: `url(${element.backdrop_path != null ? img_url + element.backdrop_path : 'https://images.pexels.com/photos/159868/lost-cat-tree-sign-fun-159868.jpeg'})` }} onClick={() => onSelect(element)}>
-                            //                             {/* bookmark */}
-                            //                             <div className='group flex justify-end pt-2 pe-2'>
-                            //                                 <div className="flex p-1 w-8 h-8 rounded-full bg-gray-700 opacity-75 cursor-pointer items-center justify-center group-hover:bg-white" onClick={(e) => { e.stopPropagation(); onBookMark(element) }}>
-                            //                                     {bookmark.find(e => e.id == element.id) ? (<RiBookmarkFill className={`group-hover:text-black  text-white `} />) : (<RiBookmarkLine className={`group-hover:text-black  text-white `} />)}
-                            //                                 </div>
-                            //                             </div>
-
-                            //                         </div>
-                            //                         {/* content */}
-                            //                         <div className='flex flex-col'>
-                            //                             <div className='flex items-center'>
-                            //                                 <p className='flex  items-center text-gray-200 py-1 md:ps-4 md:pe-2 text-xs md:text-sm px-1 '>{getYear(element.first_air_date)}</p>
-                            //                                 <hr className=" w-1 border-white border-dotted border-t-4" />
-                            //                                 <p className='flex items-center md:px-2 text-xs md:text-sm text-gray-200 capitalize'><TbDeviceTvOld className='px-1' size={26} style={{ color: 'white' }} />Tv</p>
-                            //                                 <hr className=" w-1 border-white border-dotted border-t-4 mx-1" />
-                            //                                 <p className='flex items-center md:px-2 text-xs md:text-sm text-gray-200 capitalize'>{tv_cert[element.id] || 'Loading...'}</p>
-                            //                             </div>
-                            //  <p className='flex  md:text-xl items-center text-white w-[95%]  font-medium md:px-4 pb-4 px-1 overflow-hidden text-ellipsis whitespace-nowrap' style={{
-                            //                                         overflow: 'hidden',
-                            //                                         display: '-webkit-box',
-                            //                                         WebkitBoxOrient: 'vertical',
-                            //                                         WebkitLineClamp: 2
-                            //                                     }}>
-                            //                                         {element.title}{element.name}</p>                        </div>
-                            //                     </div>
                             <BookmarkCard key={index}
                                 element={element}
                                 isMovie={false}
@@ -237,7 +181,7 @@ const Bookmarked = ({ val }) => {
                         )) : <p className='text-gray-500 my-4 text-xl  '>No TV series in bookmark</p> : <>Loading...</>}
                     </div></>) : (val?.trim() != '' ? <h2 className="text-white">No bookmarks found</h2> :
                         (<>
-                            <h2 className="text-white text-2xl lg:mx-6">Bookmarked Movies</h2>
+                            <h2 className="text-white text-2xl ">Bookmarked Movies</h2>
                             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  md:gap-4 gap-1 mt-6'>
                                 {listToRender ? listToRender.filter(e => e.media_type == "movie").length != 0 ? listToRender.filter(e => e.media_type == "movie").map((element, index) => (
 
